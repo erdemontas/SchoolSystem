65b57708400dc40801454d349070499ba8ff49a3
Erdem Ontas <e00547450@china.huawei.com>
2020-02-24 23:34:22 +0300

    Crud operations added

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,4 +1,10 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    CreateAPIView, 
+    ListAPIView, 
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 from articles.models import Article
 from .serializers import ArticleSerializer
@@ -6,7 +12,26 @@ from .serializers import ArticleSerializer
 class ArticleListView(ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    #permission_classes = (permissions.AllowAny, )
+
 
 class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    #permission_classes = (permissions.AllowAny, )
+
+
+class ArticleCreateView(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    #permission_classes = (permissions.IsAuthenticated, )
+
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    #permission_classes = (permissions.IsAuthenticated, )
+
+class ArticleDeleteView(DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    #permission_classes = (permissions.IsAuthenticated, )
